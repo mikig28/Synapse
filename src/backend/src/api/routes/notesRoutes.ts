384002ac16bcd50fe55afae14522a22fdb7ab0ad
mikig28 <mikig20@gmail.com>
2025-05-31 20:25:40 +0300

    feat(notes): Add title support for notes

--- src/backend/src/api/routes/notesRoutes.ts
+++ src/backend/src/api/routes/notesRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getNotes, createNote, deleteNote } from '../controllers/notesController';
+import { getNotes, createNote, deleteNote, updateNote } from '../controllers/notesController';
 import { protect } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.get('/', protect, getNotes);
 router.post('/', protect, createNote);
+router.put('/:id', protect, updateNote);
 router.delete('/:id', protect, deleteNote);
 
 export default router; 
